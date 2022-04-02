5e132f4499fafd2d3c7522e8e088852df946f3c2
IbnJamil <essa_muhammad@yahoo.com>
2022-04-03 03:42:23 +0500

    Default Constructor is removed from Account Controller

--- vidly/Controllers/AccountController.cs
+++ vidly/Controllers/AccountController.cs
@@ -16,10 +16,7 @@ namespace vidly.Controllers
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
-        public AccountController()
-        {
-        }
-
+       
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
         {
             UserManager = userManager;
